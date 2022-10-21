f5c18fc6dcd42a96099bef3985a2b8560cb9ad81
SamuelJAmos <79687637+SamuelJAmos@users.noreply.github.com>
2022-10-21 04:18:44 -0400

    minor bug fixes in variables

--- src/firebase.js
+++ src/firebase.js
@@ -6,10 +6,10 @@ import {getFirestore} from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: process.env.API_KEY_HIDDEN,
   authDomain: process.env.AUTH_DOMAIN_HIDDEN,
-  projectId: process.env.YOUR_PROJECT_ID,
-  storageBucket: process.env.YOUR_STORAGE_BUCKET,
-  messagingSenderId: process.env.YOUR_MESSAGING_SENDER_ID,
-  appId: process.env.YOUR_APP_ID
+  projectId: process.env.PROJECT_ID_HIDDEN,
+  storageBucket: process.env.STORAGE_BUCKET_HIDDEN,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID_HIDDEN,
+  appId: process.env.APP_ID_HIDDEN
 };
 
 const app = initializeApp(firebaseConfig);
